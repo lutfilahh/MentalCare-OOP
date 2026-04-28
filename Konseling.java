5a23e48a50b1abb0a8b23057b5267366389ade7a
Lutfilah Ahmad <lutfilahahmad169@gmail.com>
2026-04-28 14:06:25 +0700

    ngambil

--- Konseling.java
+++ Konseling.java
@@ -1,34 +1,98 @@
-import java.sql.Date;
+public class Konseling {
+    String nama;
+    String email;
 
-class MentalCaare {
-    int idKonseling, idUser, idPsikolog;
-    Date tanggalKonseling;
-    String status;
-
-    public MentalCaare(int idKonseling, int idUser, int idPsikolog, Date tanggalKonseling, String status){
-        this.idKonseling = idKonseling;
-        this.idUser = idUser;
-        this.idPsikolog = idPsikolog;
-        this.tanggalKonseling = tanggalKonseling;
-        this.status = status;
+    public Konseling(String nama, String email) {
+        this.nama = nama;
+        this.email = email;
     }
 
-    public void tampilanKonseling(){
-        System.out.println("ID Konseling:" + idKonseling);
-        System.out.println("ID User:" + idUser);
-        System.out.println("ID Psikolog:" + idPsikolog);
-        System.out.println("Tanggal Konseling:" + tanggalKonseling);
-        System.out.println("Status:" + status + "\n");
+    public String getNama() {
+        return nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("Email : " + email);
+    }  
+}
+
+class konselor extends Konseling {
+    String spesalis;
+
+    public konselor(String nama, String email, String spesalis) {
+        super(nama, email);
+        this.spesalis = spesalis;
+    }
+
+    public String getSpesalis(){
+        return spesalis;
+    }
+
+    public void tampilkanInfo(){
+        super.tampilkanInfo();
+        System.out.println("Spesalis : " + spesalis);
+    }
+
+}
+
+class klien extends Konseling {
+    String keluhan;
+
+    public klien(String nama, String email, String keluhan) {
+        super(nama, email);
+        this.keluhan = keluhan;
+    }
+
+    public String getKeluhan(){
+        return keluhan;
+    }
+
+    public void tampilaninfo(){
+        super.tampilkanInfo();
+        System.out.println("Keluhan : " + keluhan);
     }
 }
 
-public class Konseling {
-    public static void main(String[] args){
-        MentalCaare konseling1 = new MentalCaare(1, 101, 201, Date.valueOf("2024-07-01"), "Scheduled");
-        MentalCaare konseling2 = new MentalCaare(2, 102, 202, Date.valueOf("2024-07-02"), "Completed");
-        MentalCaare konseling3 = new MentalCaare(3, 103, 203, Date.valueOf("2024-07-03"), "Cancelled");
-        konseling1.tampilanKonseling();
-        konseling2.tampilanKonseling();
-        konseling3.tampilanKonseling();
+class SesiKonseling {
+    konselor konselor;
+    klien klien;
+    String tanggal;
+    boolean status;
+
+    public SesiKonseling(konselor konselor, klien klien, String tanggal, boolean status){
+        this.konselor = konselor;
+        this.klien = klien;
+        this.tanggal = tanggal;
+        this.status = status;
+    }
+
+    public void MulaiSesi() {
+        System.out.println("\n"+"========= Sesi Konseling =========");
+        System.out.println("Tanggal : " + tanggal);
+        System.out.println("Konselor : " + konselor.getNama());
+        System.out.println("Klien : " + klien.getNama());
+        System.out.println("Keluhan : " + klien.getKeluhan());
+        System.out.println("Status Sesi : " + (status ? "Selesai" : "Belum Selesai" + "\n"));
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        konselor konselor1 = new konselor("Dr Boyke", "boyke123@gmail.com","Alat kelamin");
+        klien klien1 = new klien("Aryo" ,"aryo@gmail.com" ,"Tekanan karena bermain mobile legends");
+        SesiKonseling sesi1 = new SesiKonseling(konselor1, klien1, "24 Februauri 2006", false);
+
+        System.out.println("--------------------");
+        konselor1.tampilkanInfo();
+        System.out.println("--------------------");
+        klien1.tampilaninfo();
+        System.out.println("--------------------");
+        sesi1.MulaiSesi();
     }
 }
